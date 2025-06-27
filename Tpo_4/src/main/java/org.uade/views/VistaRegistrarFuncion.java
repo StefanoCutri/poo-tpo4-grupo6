b61b8c5f79f8506d04b6894158e40958d35ef343
Stefano Cutri <stefanocutristecco@gmail.com>
2025-06-27 19:57:08 -0300

    update importaciones vistaregistrarfuncion
    fix updates

--- Tpo_4/src/main/java/org.uade/views/VistaRegistrarFuncion.java
+++ Tpo_4/src/main/java/org.uade/views/VistaRegistrarFuncion.java
@@ -8,12 +8,15 @@ import org.uade.models.Pelicula;
 import org.uade.models.Sala;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class VistaRegistrarFuncion extends JFrame {
 
